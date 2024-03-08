733eb493b2c6642b29862f33773bd4aa6fe9e410
MalikGuler <155173232+MalikGuler@users.noreply.github.com>
2024-03-08 13:18:44 +0300

    Add files via upload

---
+++ filescanner.py
@@ -0,0 +1,24 @@
+import os
+
+def list_files(directory):
+    with open("dosya_bilgileri.txt", "w") as file:
+        for root, dirs, files in os.walk(directory):
+            for dirname in dirs:
+                dir_path = os.path.join(root, dirname)
+                file.write(f"Directory: {dir_path}\n")
+                file.write("-" * 30 + "\n")
+
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                file_size = os.path.getsize(file_path)
+                file_name, file_extension = os.path.splitext(filename)
+                file.write(f"Path: {file_path}\n")
+                file.write(f"Name: {file_name}\n")
+                file.write(f"Extension: {file_extension}\n")
+                file.write(f"Size (bytes): {file_size}\n")
+                file.write("-" * 30 + "\n")
+
+if __name__ == "__main__":
+    directory = "C:\\"  # C dizinini otomatik olarak gez
+    list_files(directory)
+    print("Dosya ve klasör bilgileri başarıyla kaydedildi.")

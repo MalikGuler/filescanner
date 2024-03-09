f630aca53b429b10ba2c4350f4a34be47fe9dd5d
Taha Kara <74018704+tahakara@users.noreply.github.com>
2024-03-09 16:59:00 +0300

    Hot Fix

--- filescanner.py
+++ filescanner.py
@@ -8,15 +8,17 @@ def list_files(directory):
                 file.write(f"Directory: {dir_path}\n")
                 file.write("-" * 30 + "\n")
 
-            for filename in files:
-                file_path = os.path.join(root, filename)
-                file_size = os.path.getsize(file_path)
-                file_name, file_extension = os.path.splitext(filename)
-                file.write(f"Path: {file_path}\n")
-                file.write(f"Name: {file_name}\n")
-                file.write(f"Extension: {file_extension}\n")
-                file.write(f"Size (bytes): {file_size}\n")
-                file.write("-" * 30 + "\n")
+                # Alt dizinlerin içeriğini de dolaş
+                for sub_root, sub_dirs, sub_files in os.walk(dir_path):
+                    for filename in sub_files:
+                        file_path = os.path.join(sub_root, filename)
+                        file_size = os.path.getsize(file_path)
+                        file_name, file_extension = os.path.splitext(filename)
+                        file.write(f"Path: {file_path}\n")
+                        file.write(f"Name: {file_name}\n")
+                        file.write(f"Extension: {file_extension}\n")
+                        file.write(f"Size (bytes): {file_size}\n")
+                        file.write("-" * 30 + "\n")
 
 if __name__ == "__main__":
     directory = "C:\\"  # C dizinini otomatik olarak gez
